359a6009f43e1f2fbf3bb604a7fa3d9e7c5d762e
Abdullah <japaneseblurryabdullah@gmail.com>
2025-05-05 01:19:34 -0700

    Dashboard changes

--- frontend/src/pages/Dashboard.tsx
+++ frontend/src/pages/Dashboard.tsx
@@ -1,28 +1,41 @@
 export function Dashboard() {
     return (
-        <div className="h-screen flex flex-col">
-            <div className="h-[400px] bg-white-100 flex flex-col items-center justify-center">
-               
-                <div className="bg-gray-500 shadow-md flex gap-0">
-                <h1 className="text-3xl font-bold text-black text-center justify-center">
-                    Find Cars All Over The Platforms
+        <div className="h-screen flex flex-col items-center justify-center">
+            <div className="bg-gray-800 text-white p-6 rounded-xl shadow-md max-w-5xl flex-col mx-auto">
+                <h1 className="text-4xl font-sans+++ mb-10">
+                    Search Cars On Multiple Platforms
                 </h1>
-                    <select className="flex-1 min-w-[150px] p-4 border ">
-                        <option disabled selected>Car Make</option>
-                    </select>
-                    <select className="flex-1 min-w-[150px] p-4 border ">
-                        <option disabled selected>Car Model</option>
-                    </select>
-                    <select className="flex-1 min-w-[150px] p-4 border ">
-                        <option disabled selected>Car Year</option>
-                    </select>
-                    <select className="flex-1 min-w-[150px] p-4 border ">
-                        <option disabled selected>City</option>
-                    </select>
+                    <div className= "flex flex-wrap gap-4 items-end">
+                        <div className="flex flex-col">
+                            <label className="text-xs text-gray-300 ml-1 mb-1">Make</label>
+                            <select className="p-3 rounded-md text-blue-600 bg-white w-40">
+                                <option>Any Make</option>
+                            </select>
+                        </div>
+                        <div className="flex flex-col">
+                            <label className="text-xs text-gray-300 ml-1 mb-1">Model</label>
+                            <select className="p-3 rounded-md text-blue-600 bg-white w-40">
+                                <option>Model Name</option>
+                            </select>
+                        </div>
+                        <div className="flex flex-col">
+                            <label className="text-xs text-gray-300 ml-1 mb-1">Model Year</label>
+                            <select className="p-3 rounded-md text-blue-600 bg-white w-40">
+                                <option>Model Year</option>
+                            </select>
+                        </div>
+                        <div className="flex flex-col">
+                            <label className="text-xs text-gray-300 ml-1 mb-1">City</label>
+                            <select className="p-3 rounded-md text-blue-600 bg-white w-40">
+                                <option>City</option>
+                            </select>
+                        
+                        </div>
+                        <button className="bg-blue-600 text-white px-4 py-2 rounded-4xl p-3 w-40 h-12 ">Find Cars For me</button>
+                    </div>
                 </div>
 
             </div>
-        </div>
 
     );
 }
